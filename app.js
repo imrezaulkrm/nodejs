8ed4dd488ae5349b5e772deae3425673adfaad72
Rezaul Karim <sayem010ahmed@gmail.com>
2023-11-02 20:51:49 +0600

    update jenkins

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 // Create an HTTP server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hi i am jenkins \n');
+  res.end('hi i am jenkins. \n');
 });
 
 // Listen on port 3000
